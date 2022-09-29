db17f62e64d1b069e7422b6b50048f3a22b130b9
Nathan Dias <nathanfaria9@gmail.com>
2022-09-29 17:13:46 -0300

    Login Funcional Com JSON

--- scripts/login-page-scripts/login.js
+++ scripts/login-page-scripts/login.js
@@ -1,19 +1,20 @@
-
-// document.getElementById('login-verify').addEventListener('click', verification())
-
-
-function verification() {
+const verification = async () => {
+    const responde = await fetch("../../json/contas.json")
+    const dados = await responde.json();
     const username = document.querySelector('#email-user').value;
     const password = document.querySelector('#password-user').value;
 
-    if (username == 'admin@admin' && password == 'admin') {
-        localStorage.setItem("acesso", true);
-        window.location.href = "./../views/index.html";
-        // href="./../views/index.html"
-    } else {
-        alert('Please enter a valid username and password')
+    console.log(dados)
+
+    for (let item of dados) {
+        console.log(item.name) 
+        if (username == item.name && password == item.password) {
+            localStorage.setItem("acesso", true);
+            window.location.href = "./../views/index.html";
+        } 
     }
-}
+};
+
 
 function registrate() {
     alert("Successfully register")
